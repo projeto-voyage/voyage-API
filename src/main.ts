e9425cb94c9bcb6d8efb9778b87bb9b723262395
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2024-12-14 12:06:20 -0300

    🔧 chore(main): enable CORS based on configuration and refactor ValidationPipe setup

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,22 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    transform: true, 
-    whitelist: true,
-  }))
-
   const configService = app.get(ConfigService);
+
+  const isCorsEnabled = configService.get<boolean>('CORS_ENABLED');
+  if (isCorsEnabled) {
+    app.enableCors();
+  } else {
+    console.log('CORS disabled');
+  }
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
   const port = configService.get<number>('APP_PORT');
   await app.listen(port);
 }
