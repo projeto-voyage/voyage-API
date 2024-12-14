0488b983cae6c6c940de3b165b20f5c2982c63cc
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2024-12-13 22:50:43 -0300

    🔧  chore: Configuração de ValidationPipe, para fazer a validação de dados

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true, 
+    whitelist: true,
+  }))
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('APP_PORT');
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
