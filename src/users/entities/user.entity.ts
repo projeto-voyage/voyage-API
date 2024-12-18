ce06c0e55bb0d4c871ce0724762ad7e94e6ff300
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2024-12-17 21:28:36 -0300

    :sparkles: feat(user): create user entity and add validation via DTO

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import {
+  BeforeInsert,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+} from 'typeorm';
+import { randomUUID } from 'node:crypto';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @BeforeInsert()
+  generateId() {
+    this.id = randomUUID();
+  }
+}
