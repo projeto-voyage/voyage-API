aa15dbb7e9af86196f4e5e1ce21fdec1debef39d
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-03 18:30:39 -0300

    :sparkles: feat(itinerary, trip): add JWT authentication guard to itinerary and trip controllers

--- src/itinerary/itinerary.controller.ts
+++ src/itinerary/itinerary.controller.ts
@@ -6,30 +6,36 @@ import {
   Delete,
   Param,
   Body,
+  UseGuards,
 } from '@nestjs/common';
 import { ItineraryService } from './itinerary.service';
 import { CreateItineraryDto } from './dto/create-itinerary.dto';
 import { UpdateItineraryDto } from './dto/update-itinerary.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('itineraries')
 export class ItineraryController {
   constructor(private readonly itineraryService: ItineraryService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createItineraryDto: CreateItineraryDto) {
     return this.itineraryService.createItinerary(createItineraryDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.itineraryService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findById(@Param('id') id: string) {
     return this.itineraryService.findById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(
     @Param('id') id: string,
@@ -38,6 +44,7 @@ export class ItineraryController {
     return this.itineraryService.updateItinerary(id, updateItineraryDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   delete(@Param('id') id: string) {
     return this.itineraryService.deleteItinerary(id);
--- src/trip/trip.controller.ts
+++ src/trip/trip.controller.ts
@@ -6,35 +6,42 @@ import {
   Delete,
   Param,
   Body,
+  UseGuards,
 } from '@nestjs/common';
 import { TripService } from './trip.service';
 import { CreateTripDto } from './dto/create-trip.dto';
 import { UpdateTripDto } from './dto/update-trip.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('trips')
 export class TripController {
   constructor(private readonly tripService: TripService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createTripDto: CreateTripDto) {
     return this.tripService.createTrip(createTripDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.tripService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findById(@Param('id') id: string) {
     return this.tripService.findById(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(@Param('id') id: string, @Body() updateTripDto: UpdateTripDto) {
     return this.tripService.updateTrip(id, updateTripDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   delete(@Param('id') id: string) {
     return this.tripService.deleteTrip(id);
