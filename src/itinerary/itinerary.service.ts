4a5c43802ddff97a17887b78a4cb6d28e414d86d
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-04 15:30:27 -0300

    :sparkles: feat(itinerary): enhance file handling in ItineraryService, switch to async file operations and update storage path

--- src/itinerary/itinerary.service.ts
+++ src/itinerary/itinerary.service.ts
@@ -4,11 +4,14 @@ import { Repository } from 'typeorm';
 import { Itinerary } from './entities/itinerary.entity';
 import { UpdateItineraryDto } from './dto/update-itinerary.dto';
 import { GeminiService } from 'src/gemini/gemini.service';
-import * as fs from 'fs';
+import * as fs from 'fs/promises';
 import * as path from 'path';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class ItineraryService {
+  private readonly storageDir = path.join(process.cwd(), 'storage/itineraries');
+
   constructor(
     @InjectRepository(Itinerary)
     private readonly itineraryRepository: Repository<Itinerary>,
@@ -26,23 +29,20 @@ export class ItineraryService {
     const content = await this.geminiService.genarateItinerary(prompt);
 
     // Criar diretório se não existir
-    const dirPath = path.join(__dirname, '../../itineraries');
-    if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath, { recursive: true });
-    }
+    await fs.mkdir(this.storageDir, { recursive: true });
 
     // Nome do arquivo (UUID)
-    const fileName = `${crypto.randomUUID()}.txt`;
-    const filePath = path.join(dirPath, fileName);
+    const fileName = `${randomUUID()}.txt`;
+    const filePath = path.join(this.storageDir, fileName);
 
     // Salvar conteúdo no arquivo
-    fs.writeFileSync(filePath, content);
+    await fs.writeFile(filePath, content, 'utf8');
 
     const itinerary = this.itineraryRepository.create({
       destination,
       totalDays,
       totalCost,
-      filePath, // Armazena apenas o caminho
+      filePath, 
     });
 
     return this.itineraryRepository.save(itinerary);
@@ -64,10 +64,10 @@ export class ItineraryService {
     const itinerary = await this.findById(id);
 
     // Verifica se o arquivo existe antes de ler
-    if (!fs.existsSync(itinerary.filePath)) {
+    try {
+      return await fs.readFile(itinerary.filePath, 'utf8');
+    } catch (error) {
       throw new NotFoundException('Arquivo de roteiro não encontrado.');
     }
-
-    return fs.readFileSync(itinerary.filePath, 'utf8');
   }
 }
