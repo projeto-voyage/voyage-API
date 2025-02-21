445b5ab6b006aa26fa1ed4f74ad218dbe841328e
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-21 15:51:59 -0300

    :sparkles: feat(auth): remove Google authentication guard and strategy; enhance multi-auth guard with logging

--- src/auth/guards/multi-auth.guard.ts
+++ src/auth/guards/multi-auth.guard.ts
@@ -1,16 +1,20 @@
-import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { Injectable, ExecutionContext, UnauthorizedException, Logger } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class MultiAuthGuard extends AuthGuard(['jwt', 'google']) {
+  private readonly logger = new Logger(MultiAuthGuard.name);
+
   canActivate(context: ExecutionContext) {
     return super.canActivate(context);
   }
 
-  handleRequest(err, user) {
+  handleRequest(err, user, info) {
     if (err || !user) {
+      this.logger.error(`Erro de autenticação: ${info?.message || 'Usuário não encontrado'}`);
       throw err || new UnauthorizedException();
     }
+    this.logger.log(`Usuário autenticado: ${JSON.stringify(user)}`);
     return user;
   }
 }
