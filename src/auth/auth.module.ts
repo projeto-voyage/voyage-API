1b338b85160de7eba985d99d134114978327fa96
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-22 21:01:08 -0300

    ``` No code changes detected; skipping commit. ```

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,12 +6,11 @@ import { JwtStrategy } from './strategy/jwt.strategy';
 import { UsersModule } from 'src/users/users.module';
 import { EnvModule } from 'src/env/env.module';
 import { JwtConfModule } from './jwt-conf/jwt-conf.module';
-import { GoogleStrategy } from './strategy/google.strategy';
 
 @Module({
   imports: [UsersModule, PassportModule, EnvModule, JwtConfModule],
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy, GoogleStrategy],
+  providers: [AuthService, JwtStrategy],
   exports: [JwtStrategy],
 })
 export class AuthModule {}
